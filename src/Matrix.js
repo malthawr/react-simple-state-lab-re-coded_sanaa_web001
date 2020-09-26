69d5b7f649a4a841afb97dbacbcae044dba288a5
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-26 12:11:15 +0000

    Automatically backed up by Learn

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,29 +1,36 @@
 import React, { Component } from 'react';
-import Cell from './Cell.js'
+import Cell from './Cell';
 
 export default class Matrix extends Component {
-  
-  genRow = (vals) => {
-    return vals.map(val => <Cell value={val} />)
-  }
-  
-  genMatrix = () => {
-    return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
-  }
-  
-  render() {
-    return (
-      <div id="matrix">
-        {this.genMatrix()}
-      </div>
-    )
-  }
-  
-}
 
+  genRow = (vals) => {
+    return vals.map(val =><Cell value={val} />) // replace me and render a cell component instead!
+ }
+
+ genMatrix = () => {
+   return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+ }
+ 
+ render() {
+   return (
+     <div id="matrix">
+       {this.genMatrix()}
+     </div>
+   )
+ }
+
+}
 Matrix.defaultProps = {
-  values: (() => {
-    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
-    return (new Array(10).fill(defRow))
-  })()
+ values: [
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+   ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+ ]
 }
